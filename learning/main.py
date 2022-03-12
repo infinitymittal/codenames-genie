a883bb1f1654010c6b97755d69edc092055fa3c7
Anant Mittal <infinitymittal@gmail.com>
2022-03-12 14:49:08 -0800

    checking for multiple word

--- learning/main.py
+++ learning/main.py
@@ -5,18 +5,21 @@ board_words = set(["casino", "shot", "spurs", "blacksmith", "lawyer",\
                    "hook", "pepper", "cheese", "robin", "page",\
                    "rust", "ball", "peanut", "key", "mole",\
                    "van", "sack", "piano", "santa", "screen"])
-guess_word = "metal"
+guess_words = set(["metal", "money", "animal"])
 
 all_words = set(board_words)
-all_words.add(guess_word)
+all_words.update(guess_words)
 
-def find_closest(embed_obj, word):
-    closest = embed_obj.find_closest_by_word(word)
-    #closest = [word for word in closest if word[0] in board_words]
+def find_closest(embed_obj, guess_word):
+    closest = embed_obj.find_closest_by_word(guess_word)
+    closest = [word for word in closest if word[0] in board_words]
     return closest
 
 fasttext = FastText(all_words)
-result = find_closest(fasttext, guess_word)
-for word,score in result:
-    print(word, score, sep='\t')
-
+for guess_word in guess_words:
+    print("checking for guess {}.".format(guess_word))
+    result = find_closest(fasttext, guess_word)
+    for word,score in result:
+        print(word, score, sep='\t')
+    print()
+    
